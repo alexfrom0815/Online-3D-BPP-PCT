a89afbf21da852fe61170b2a5a39ca2ee875fb07
Hang Zhao <47166429+alexfrom0815@users.noreply.github.com>
2023-11-06 15:27:08 +0800

    Update evaluation.py and fix a eval bug

--- evaluation.py
+++ evaluation.py
@@ -33,7 +33,11 @@ def main(args):
                     internal_node_holder=args.internal_node_holder,
                     leaf_node_holder=args.leaf_node_holder,
                     LNES = args.lnes,
-                    shuffle=args.shuffle)
+                    shuffle=args.shuffle
+                    sample_from_distribution=args.sample_from_distribution,
+                    sample_left_bound=args.sample_left_bound,
+                    sample_right_bound=args.sample_right_bound
+                   )
 
     # Create the main actor & critic networks of PCT
     PCT_policy =  DRL_GAT(args)
