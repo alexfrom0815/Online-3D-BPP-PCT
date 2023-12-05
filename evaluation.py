b5cc8b8eae65a7b326804b752c86f5b0d5e9a760
beiguagua <1248074016@qq.com>
2023-12-05 19:22:50 +0800

    Update evaluation.py and fix a eval bug: missing comma

--- evaluation.py
+++ evaluation.py
@@ -33,7 +33,7 @@ def main(args):
                     internal_node_holder=args.internal_node_holder,
                     leaf_node_holder=args.leaf_node_holder,
                     LNES = args.lnes,
-                    shuffle=args.shuffle
+                    shuffle=args.shuffle,
                     sample_from_distribution=args.sample_from_distribution,
                     sample_left_bound=args.sample_left_bound,
                     sample_right_bound=args.sample_right_bound
